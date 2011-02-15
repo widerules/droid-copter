967ea280da8589dc3fc5baaf02da6b88067a4e75
daniellmorris <daniellmorris@3527f968-f980-8be7-8f40-d89ad1c2a5ec>
2011-02-15 01:48:05 +0000

    DLM - modify passed in parameters

--- trunk/quadcopter-android/src/com/quadcopter/webserver/servlets/TestServlet.java
+++ trunk/quadcopter-android/src/com/quadcopter/webserver/servlets/TestServlet.java
@@ -1,13 +1,14 @@
 package com.quadcopter.webserver.servlets;
 
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.RequestLine;
+import org.apache.http.impl.DefaultHttpServerConnection;
 
 public class TestServlet extends Servlet 
 {
 	@Override
-	public HttpResponse runServlet(RequestLine requestLine) {
-		String body = "This is a test servlet - uri = \"" + requestLine.getUri() + "\"";
+	public HttpResponse runServlet(DefaultHttpServerConnection serverConnection, HttpRequest request) {
+		String body = "This is a test servlet - uri = \"" + request.getRequestLine().getUri() + "\"";
 		
 		return getHTTPSuccessResponse(body);
 	}
