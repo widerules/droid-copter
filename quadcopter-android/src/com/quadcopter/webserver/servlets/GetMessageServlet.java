13ae66d67a0c751af3ce152d6325038c54f25106
daniellmorris <daniellmorris@3527f968-f980-8be7-8f40-d89ad1c2a5ec>
2011-02-16 19:58:22 +0000

    DLM - initial add. This is a sample for bluetooth communication

---
+++ quadcopter-android/src/com/quadcopter/webserver/servlets/GetMessageServlet.java
@@ -0,0 +1,50 @@
+package com.quadcopter.webserver.servlets;
+
+import java.util.LinkedList;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.DefaultHttpServerConnection;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import com.quadcopter.background.hardware.BluetoothCommunication;
+
+public class GetMessageServlet extends Servlet{
+
+	LinkedList<String> msgQueue = new LinkedList<String>();
+	
+	BroadcastReceiver receiver = new BroadcastReceiver() {
+		@Override
+		public synchronized void onReceive(Context context, Intent intent) {
+			if (intent.getAction().equals(BluetoothCommunication.BROADCAST_BLUETOOTH_RECIEVCED_MESSAGE))
+			{
+				String msg = intent.getStringExtra(BluetoothCommunication.EXTRA_STRING_DATA_TO_SEND);
+				if (msg!=null)
+				{
+					msgQueue.add(msg);
+				}
+			}
+		}
+	};
+	
+	@Override
+	public void setupServlet(Context context) {
+		super.setupServlet(context);
+		IntentFilter filter = new IntentFilter(BluetoothCommunication.BROADCAST_BLUETOOTH_RECIEVCED_MESSAGE);
+		context.registerReceiver(receiver, filter);
+	}
+
+	@Override
+	public synchronized HttpResponse runServlet(DefaultHttpServerConnection serverConnection, HttpRequest request) 
+	{
+		String ret="";
+		while (!msgQueue.isEmpty())
+			ret += msgQueue.poll() + "\n";
+		return getHTTPSuccessResponse(ret);
+	}
+
+}
