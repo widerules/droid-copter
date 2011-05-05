d5f19fe03767bd74718906fc9aee134aebb76900
daniellmorris <daniellmorris@3527f968-f980-8be7-8f40-d89ad1c2a5ec>
2011-05-05 05:13:43 +0000

    DLM - final changes

--- trunk/quadcopter-android/src/com/quadcopter/background/hardware/GPSRefresher.java
+++ trunk/quadcopter-android/src/com/quadcopter/background/hardware/GPSRefresher.java
@@ -5,9 +5,15 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.util.Log;
 
 public class GPSRefresher 
 {
+	private static final String TAG = "GPSRefresher"; 
+	
 	private Context mContext = null;
 	
 	private LocationManager locationManager = null;
@@ -15,52 +21,88 @@ public class GPSRefresher
 	//one min
 	private static final int MIN_UPDATE_TIME = 1000 * 60 * 1;
 	
-	private Location currentLocation = null;
+	private HandlerThread refreshThread = null;
+	
+	private static Location currentLocation = null;
 	
 	// Define a listener that responds to location updates
 	LocationListener locationListener = new LocationListener() 
 	{	
 		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {}
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			Log.i(TAG, "GPS status changed");
+		}
 		@Override
-		public void onProviderEnabled(String provider) {}
+		public void onProviderEnabled(String provider) {
+			Log.i(TAG, "GPS Provide enabled");
+		}
 		@Override
-		public void onProviderDisabled(String provider) {}
+		public void onProviderDisabled(String provider) {
+			Log.i(TAG, "GPS provider disabled");
+		}
 		@Override
 		public void onLocationChanged(Location location) {
+			Log.i(TAG, "GPS location changed");
 		   // Called when a new location is found by the network location provider.
-			setCurrentLocation(location);
+			if (location!=null)
+				setCurrentLocation(location);
 		}
 	};
 	
 	public GPSRefresher(Context context)
 	{
 		this.mContext = context;
-		// Acquire a reference to the system Location Manager
-		locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
 	}
 	
 	public void start()
 	{
-		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+		// Acquire a reference to the system Location Manager
+		locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+		
+		if (locationManager!=null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
 		{
 			locationManager.removeUpdates(locationListener);
 		}
-		// Register the listener with the Location Manager to receive location updates
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_UPDATE_TIME, 0, locationListener);
+		setCurrentLocation(locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER));
+
+//		refreshThread = new HandlerThread("GPSRefresher")
+//		{
+//			// Or use LocationManager.GPS_PROVIDER
+//			@Override
+//			public void run() {
+//				GPSRefresher.this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_UPDATE_TIME, 0, GPSRefresher.this.locationListener);
+//			}	
+//		};
+		 refreshThread = new HandlerThread("GPS Thread");
+         refreshThread.start();
+         new Handler(refreshThread.getLooper()).post(
+             new Runnable() {
+				 @Override
+				 public void run() {
+					 GPSRefresher.this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, GPSRefresher.this.locationListener);
+				 }
+             }
+         );
 	}
 	
 	public void stop()
 	{
 		// Remove the listener you previously added
-		locationManager.removeUpdates(locationListener);
+		if (locationManager!=null)
+			locationManager.removeUpdates(locationListener);
+		refreshThread.getLooper().quit();
 	}
 
-	public void setCurrentLocation(Location currentLocation) {
-		this.currentLocation = currentLocation;
+	public static synchronized void setCurrentLocation(Location currentLocation) {
+		if (currentLocation!=null)
+		{
+			Log.i(TAG, "Latitude=" + currentLocation.getLatitude()
+								+ ", Longitude=" + currentLocation.getLongitude());
+			GPSRefresher.currentLocation = currentLocation;
+		}
 	}
 
-	public Location getCurrentLocation() {
+	public static synchronized Location getCurrentLocation() {
 		return currentLocation;
 	}
 }
